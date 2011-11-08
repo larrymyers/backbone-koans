3f5043b127eb95af910455b78c34adc5b8ee4aff
Larry Myers <larry@larrymyers.com>
2011-11-08 12:29:35 -0500

    working validation and specs for Model

--- koans/aboutModels.js
+++ koans/aboutModels.js
@@ -21,7 +21,36 @@ describe('About Backbone.Model', function() {
     it('Will call a custom initialize function on the model instance when created.', function() {
         var toot = new Tooter.Toot({ user: 'toad' });
         
+        // FIX ME change to 'toad' to fail it
         expect(toot.get('user')).toBe('Toad');
     });
     
+    it('Fires a custom event when the state changes.', function() {
+        var spy = jasmine.createSpy();
+        
+        var toot = new Tooter.Toot();
+        
+        toot.bind('change', spy);
+        
+        // FIX ME remove this line to fail it.
+        toot.set({ 'message': 'Looking for a tanooki suit!' });
+        
+        expect(spy).toHaveBeenCalled();
+    });
+    
+    it('Can contain custom validation rules, and will trigger an error event on failed validation.', function() {
+        var spy = jasmine.createSpy();
+        
+        var toot = new Tooter.Toot();
+        
+        toot.bind('error', spy);
+        
+        // FIX ME remove this line to fail it.
+        toot.set({ 'message': '' });
+        
+        var errorArgs = spy.mostRecentCall.args;
+        
+        expect(errorArgs[0]).toBe(toot);
+        expect(errorArgs[1]).toBe('Must contain a message.');
+    });
 });
--- tooterApp.js
+++ tooterApp.js
@@ -16,6 +16,20 @@
             userName = userName.slice(0,1).toUpperCase() + userName.slice(1).toLowerCase();
             
             this.set({ user: userName }, { silent: true });
+        },
+        
+        validate: function(attrs) {
+            if (_.isString(attrs.user) && attrs.user.length === 0) {
+                return 'Must have a user.';
+            }
+            
+            if (_.isString(attrs.message) && attrs.message.length === 0) {
+                return 'Must contain a message.';
+            }
         }
+    });
+    
+    Tooter.Toots = Backbone.Collection.extend({
+        model: Tooter.Toot
     });
 })();
