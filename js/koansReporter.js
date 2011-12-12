a8fd9195c64c5bc507d6deece4db749e189541a9
Larry Myers <larry@larrymyers.com>
2011-12-12 09:01:17 -0500

    start rendering out runner results with custom reporter

--- js/koansReporter.js
+++ js/koansReporter.js
@@ -10,7 +10,60 @@
         
         render: function() {
             var runner = this.options.runner,
-                results = runner.results();
+                suites = runner.suites(),
+                suite, results, suiteHTML, i;
+            
+            $(this.el).empty();
+            
+            for (i = 0; i < suites.length; i++) {
+                suite = suites[i];
+                results = suite.results();
+                suiteHTML = '<div>' + suite.description + ': ' +  results.passedCount + ' of ' + results.totalCount + '</div>';
+                $(this.el).append(suiteHTML);
+            }
+            
+            return this;
+        }
+    });
+    
+    var ProgressView = Backbone.View.extend({
+        el: '#progress',
+        
+        initialize: function(options) {
+            options.runner.bind('finished', this.render, this);
+        },
+        
+        render: function() {
+            var runner = this.options.runner,
+                suites = runner.suites(),
+                suite, items, specs, spec, i, j, suiteElt, specElt;
+            
+            if (KoansFailure) {
+                return this;
+            }
+            
+            for (i = 0; i < suites.length; i++) {
+                suite = suites[i];
+                specs = suite.specs();
+                
+                suiteElt = this.make('h2', {}, suite.description);
+                $(this.el).append(suiteElt);
+                
+                for (j = 0; j < specs.length; j++) {
+                    spec = specs[j];
+                    results = spec.results();
+                    
+                    var specStyle = (results.failedCount > 0) ? 'failed' : 'passed';
+                    
+                    specElt = this.make('div', {className: specStyle}, spec.description);
+                    $(this.el).append(specElt);
+                    
+                    if (results.failedCount > 0) {
+                        KoansFailure = true;
+                        return this;
+                    }
+                }
+            }
             
             return this;
         }
@@ -22,11 +75,11 @@
     
     _.extend(KoansReporter.prototype, {
         reportRunnerStarting: function(runner) {
-            _.extend(runner, Backbone.Events);
-            var summary = new SummaryView({runner: runner});
+            window.KoansFailure = false;
             
-            var suites = runner.suites();
-            var specs = runner.specs();
+            _.extend(runner, Backbone.Events);
+            new SummaryView({runner: runner});
+            new ProgressView({runner: runner});
         },
         
         reportRunnerResults: function(runner) {
