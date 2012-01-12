b1b38119b5bb6c08dcee0f318a230d33e4098aa2
Larry Myers <larry@larrymyers.com>
2012-01-12 14:44:41 -0500

    redo Backbone.Event spec so that it can be passed without having to change a single expectation

--- js/koans/aboutEvents.js
+++ js/koans/aboutEvents.js
@@ -10,15 +10,16 @@ describe('About Backbone.Events', function() {
     it('Can extend javascript objects to support custom events.', function() {
         var basicObject = {};
         
-        _.extend(basicObject, Backbone.Events);
+        // How would you give basicObject these functions?
+        // Hint: http://documentcloud.github.com/backbone/#Events
         
-        expect(basicObject.bind).not.toBeDefined();
-        expect(basicObject.unbind).not.toBeDefined();
-        expect(basicObject.trigger).not.toBeDefined();
+        expect(typeof basicObject.bind).toEqual('function');
+        expect(typeof basicObject.unbind).toEqual('function');
+        expect(typeof basicObject.trigger).toEqual('function');
     });
     
     it('Allows us to bind and trigger custom named events on an object.', function() {
-        var callback = jasmine.createSpy();
+        var callback = jasmine.createSpy('-Custom Event Callback-');
         
         obj.bind('basic event', callback);
         
@@ -36,9 +37,9 @@ describe('About Backbone.Events', function() {
             }
         });
         
-        obj.trigger('some event', 1, 2);
+        obj.trigger('some event');
         
-        expect(passedArgs.length).toBe(3);
+        expect(passedArgs).toEqual(['arg1', 'arg2']);
     });
     
     it('Can also bind the passed context to the event callback.', function() {
@@ -48,50 +49,52 @@ describe('About Backbone.Events', function() {
             this.color = 'red';
         }
         
-        obj.bind('an event', changeColor, foo);
+        // How would you get 'this.color' to refer to 'foo' in the changeColor function?
+        
+        obj.bind('an event', changeColor);
         
         obj.trigger('an event');
         
-        expect(foo.color).toBe('blue');
+        expect(foo.color).toEqual('red');
     });
     
     it("Uses 'all' as a special event name to capture all events bound to the object.", function() {
-        var callback = jasmine.createSpy();
+        var callback = jasmine.createSpy('-Custom Event Callback-');
         
         obj.bind('all', callback);
         
-        obj.trigger('foo');
-        obj.trigger('bar');
-        
-        expect(callback.callCount).toBe(0);
-        expect(callback.mostRecentCall.args[0]).toBe(undefined);
+        expect(callback.callCount).toBe(1);
+        expect(callback.mostRecentCall.args[0]).toBe('custom event');
     });
     
     it('Also can remove custom events from objects.', function() {
-        var spy1 = jasmine.createSpy();
-        var spy2 = jasmine.createSpy();
+        var spy1 = jasmine.createSpy('-Spy 1-');
+        var spy2 = jasmine.createSpy('-Spy 2-');
+        var spy3 = jasmine.createSpy('-Spy 3-');
          
         obj.bind('foo', spy1);
         obj.bind('foo', spy2);
+        obj.bind('foo', spy3);
         obj.bind('bar', spy1);
         
-        // We can unbind just a reference to a bound callback for the event.
-        obj.unbind('foo', spy1);
+        // How do you unbind just a single callback for the event?
+        
         obj.trigger('foo');
         
-        expect(spy1).toHaveBeenCalled();
+        expect(spy1).not.toHaveBeenCalled();
         
-        // We can unbind all callbacks tied to the event.
-        obj.unbind('foo');
+        // How do you unbind all callbacks tied to the event with a single method?
+        
         obj.trigger('foo');
         
-        expect(spy2.callCount).toBe(2);
+        expect(spy2.callCount).toEqual(1);
+        expect(spy3.callCount).toEqual(1);
         
-        // We can unbind all events bound to the object.
-        obj.unbind();
+        // How do you unbind all callbacks and events tied to the object with a single method?
+        
         obj.trigger('bar');
         
-        expect(spy1).toHaveBeenCalled();
+        expect(spy1).not.toHaveBeenCalled();
     });
     
 });
