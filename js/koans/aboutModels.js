bbb50084dabf1932ee9d3a3e4526cc1b410f138c
Larry Myers <larry@larrymyers.com>
2012-01-15 23:09:27 -0500

    updated model spec to not require changes to any of the expect statements

--- js/koans/aboutModels.js
+++ js/koans/aboutModels.js
@@ -3,34 +3,40 @@ describe('About Backbone.Model', function() {
     it('Can be created with default values for its attributes.', function() {
         var todo = new Todo();
         
-        expect(todo.get('text')).toBe("What should the default be here?");
+        var defaultAttrs = {
+            text: 'What is the default value?',
+            done : 'What is the default value?',
+            order: 'What is the default value?'
+        }
+        
+        expect(todo.attributes).toEqual(defaultAttrs);
     });
     
     it('Will set passed attributes on the model instance when created.', function() {
         var todo = new Todo({ text: 'Get oil change for car.' });
         
-        expect(todo.get('text')).toBe("FIX ME");
-        expect(todo.get('done')).toBe("FIX ME");
-        expect(todo.get('order')).toBe("FIX ME");
+        var expectedText = 'FIX ME';
+        
+        expect(todo.get('text')).toEqual(expectedText);
     });
     
     it('Will call a custom initialize function on the model instance when created.', function() {
-        var toot = new Todo({ text: 'Stop monkeys from throwing their own crap!' });
+        var todo = new Todo({ text: 'Stop monkeys from throwing their own feces!' });
         
-        expect(toot.get('text')).toBe('Stop monkeys from throwing their own crap!');
+        expect(todo.get('text')).toBe('Stop monkeys from throwing their own rainbows!');
     });
     
     it('Fires a custom event when the state changes.', function() {
-        var spy = jasmine.createSpy('-change event callback-');
+        var callback = jasmine.createSpy('-change event callback-');
         
         var todo = new Todo();
         
-        todo.bind('change', spy);
+        todo.bind('change', callback);
         
         // How would you update a property on the todo here?
         // Hint: http://documentcloud.github.com/backbone/#Model-set
         
-        expect(spy).toHaveBeenCalled();
+        expect(callback).toHaveBeenCalled();
     });
     
     it('Can contain custom validation rules, and will trigger an error event on failed validation.', function() {
